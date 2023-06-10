cdbe0f9590c16acbe0aa17135b53744fff699d5a
joaquinPuente <joaquinpuentecoronel@gmail.com>
2023-06-10 05:50:35 -0300

    segundo desafio

--- Desafios/ProductManager2.js
+++ Desafios/ProductManager2.js
@@ -7,9 +7,13 @@ class ProductManager {
         this.filename = filename;
         this.format = 'utf-8';
         this.ultID = 0;
+        this.path = './product.json'
     }
 
-    
+    createID () {
+        this.ultID++;
+        return this.ultID;
+    }
 
     addProducts = async (title, description, price, thumbnail, code, stock) => {
         const productID = this.createID();
@@ -30,17 +34,33 @@ class ProductManager {
       const dataObj = JSON.parse(data);
 
       return dataObj;
-    } catch (err) {
+    } 
+    catch (err) {
       //Sino existe el archivo
       console.log(`Error: ${err}`);
       return [];
     }
     };
 
-    createID () {
-        this.ultID++;
-        return this.ultID;
-    }
+    getProductsByID = async (id) => {
+        try {
+            const data = await fs.promises.readFile(this.filename, this.format);
+            const products = JSON.parse(data);
+    
+            for (const product of products) {
+                if (product.id === id) {
+                    return product;
+                }
+            }
+    
+            console.log("ID no encontrado");
+            return null;
+        } 
+        catch (err) {
+            console.log(`Error: ${err}`);
+            return null;
+        }
+    };
 
 }
 
@@ -51,6 +71,6 @@ run = async() => {
     await products.addProducts("producto prueba3","Este es un producto prueba",200,"Sin imagen","abc123",25)
 
     console.log(await products.getProducts());
+    console.log("id buscado:  ",await products.getProductsByID(2)) 
 };
-
-run() 
+run();
